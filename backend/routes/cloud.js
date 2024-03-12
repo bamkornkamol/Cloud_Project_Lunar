3126cbe40e7783994f04416d1dd2fd22a16fb264
bamkornkamol <88421920+bamkornkamol@users.noreply.github.com>
2024-03-13 03:46:29 +0700

    edit transaction

--- backend/routes/cloud.js
+++ backend/routes/cloud.js
@@ -16,16 +16,24 @@ var storage = multer.diskStorage({
 })
 const upload = multer({ storage: storage })
 
-router.post('/RegisUser',upload.single(), async function(req, res, next){
+router.post('/RegisUser',upload.single(), async (req, res, next) => {
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
+
     try{
-        const result = await pool.query(
+        await pool.query(
             "insert into user(fname, lname, email, password, phone) VALUES(?, ?, ?, ?, ?)",
             [req.body.fname, req.body.lname, req.body.email, req.body.password, req.body.phone]
         );
-        console.log('success')
-        return res.json(result)
-    }catch (err){
+        conn.commit()
+        res.status(200).send('success')
+    }catch(err){
+        await conn.rollback()
         console.log(err)
+        return res.status(404).send('add error')
+    }finally{
+        conn.release()
+        console.log('finally')
     }
 })
 
@@ -51,7 +59,7 @@ router.post('/Login', upload.single(), async(req, res) => {
     }
 })
 
-router.get('/myDormitory/:userId',upload.single(), async function(req, res, next){
+router.get('/myDormitory/:userId',upload.single(), async (req, res, next) => {
     try{
         const result = await pool.query(
             `select * from dormitory where user_id = ?`, [req.params.userId]
@@ -69,7 +77,7 @@ router.get('/myDormitory/:userId',upload.single(), async function(req, res, next
     }
 })
 
-router.get('/user/:userId',upload.single(), async function(req, res, next){
+router.get('/user/:userId',upload.single(), async (req, res, next) => {
     try{
         const result = await pool.query(
             `select * from user where id = ?`, [req.params.userId]
@@ -85,21 +93,30 @@ router.get('/user/:userId',upload.single(), async function(req, res, next){
 
 // dormitory ==========================================================================================================================
 
-router.post('/RegisDor/:userId',upload.single(), async function(req, res, next){
+router.post('/RegisDor/:userId',upload.single(), async (req, res, next) => {
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
+
     try{
-        const result = await pool.query(
+        await pool.query(
             "insert into dormitory(user_id, name, address, province, district, parish, post, phone, room, floor, water, light, duedate) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
             [req.params.userId, req.body.name, req.body.address, req.body.province, req.body.district, req.body.parish, req.body.post, req.body.phone,
             req.body.room, req.body.floor, req.body.water, req.body.light, req.body.duedate]
         );
-        console.log('success')
-        return res.json(result)
-    }catch (err){
+        
+        conn.commit()
+        res.status(200).send('success')
+    }catch(err){
+        await conn.rollback()
         console.log(err)
+        return res.status(404).send('add error')
+    }finally{
+        conn.release()
+        console.log('finally')
     }
 })
 
-router.get('/Dormitory/:userId/:dorId',upload.single(), async function(req, res, next){
+router.get('/Dormitory/:userId/:dorId',upload.single(), async (req, res, next) => {
     try{
         const result = await pool.query(
             `select * from dormitory where id = ? and user_id = ?`, [req.params.dorId, req.params.userId]
@@ -116,29 +133,47 @@ router.get('/Dormitory/:userId/:dorId',upload.single(), async function(req, res,
     }
 })
 
-router.put('/editDormitory/:userId/:dorId',upload.single(), async function(req, res, next){
+router.put('/editDormitory/:userId/:dorId',upload.single(), async (req, res, next)=>{
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
+
     try{
-        const result = await pool.query(
+        await pool.query(
             `update dormitory set name=?, address=?, province=?, district=?, parish=?, post=?, phone=?, room=?, floor=?, water=?, light=?, duedate=? 
             where id = ? and user_id = ?`, [req.body.name, req.body.address, req.body.province, req.body.district, req.body.parish, req.body.post, req.body.phone,
                 req.body.room, req.body.floor, req.body.water, req.body.light, req.body.duedate, req.params.dorId, req.params.userId]
         );
-        console.log('success')
-        return res.json(result)
-    }catch (err){
+        
+        conn.commit()
+        res.status(200).send('success')
+    }catch(err){
+        await conn.rollback()
         console.log(err)
+        return res.status(404).send('add error')
+    }finally{
+        conn.release()
+        console.log('finally')
     }
 })
 
-router.delete('/deleteDormitory/:userId/:dorId', async function(req, res, next){
+router.delete('/deleteDormitory/:userId/:dorId', async (req, res, next)=>{
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
+
     try{
-        const result = await pool.query(
+        await pool.query(
             `delete from dormitory where id = ? and user_id = ?`, [req.params.dorId, req.params.userId]
         );
-        console.log('success')
-        return res.json(result)
-    }catch (err){
+        
+        conn.commit()
+        res.status(200).send('success')
+    }catch(err){
+        await conn.rollback()
         console.log(err)
+        return res.status(404).send('add error')
+    }finally{
+        conn.release()
+        console.log('finally')
     }
 })
 
@@ -169,7 +204,7 @@ router.post('/addRenter/:userId/:dorId',upload.single(), async (req, res, next)=
     }
 })
 
-router.get('/Renter/:userId/:dorId',upload.single(), async function(req, res, next){
+router.get('/Renter/:userId/:dorId',upload.single(), async (req, res, next) => {
     try{
         const result = await pool.query(
             `select * from renter where dor_id = ? ORDER BY num_room`, [req.params.dorId]
@@ -182,7 +217,7 @@ router.get('/Renter/:userId/:dorId',upload.single(), async function(req, res, ne
     }
 })
 
-router.get('/detailRenter/:userId/:dorId/:rentId',upload.single(), async function(req, res, next){
+router.get('/detailRenter/:userId/:dorId/:rentId',upload.single(), async (req, res, next) => {
     try{
         const result = await pool.query(
             `select * from renter where id = ? and dor_id = ?`, [req.params.rentId, req.params.dorId]
@@ -195,38 +230,56 @@ router.get('/detailRenter/:userId/:dorId/:rentId',upload.single(), async functio
     }
 })
 
-router.put('/editRenter/:userId/:dorId/:rentId',upload.single(), async function(req, res, next){
+router.put('/editRenter/:userId/:dorId/:rentId',upload.single(), async (req, res, next) => {
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
+
     try{
-        const pay = await pool.query(
+        await pool.query(
             `UPDATE payment SET num_room=? where dor_id = ? and renter_id =?`, 
             [req.body.num_room, req.params.dorId, req.params.rentId]
         );
 
-        const result = await pool.query(
+        await pool.query(
             `UPDATE renter SET  num_room=?,  type=?, price=?, email=?, name1=?, phone1=?, name2=?, phone2=? WHERE id = ? and dor_id = ?`, 
             [req.body.num_room, req.body.type, req.body.price, req.body.email, req.body.name1, req.body.phone1, req.body.name2,
                 req.body.phone2, req.params.rentId, req.params.dorId]
         );
-        console.log('success')
-        return res.json(result)
-    }catch (err){
+
+        conn.commit()
+        res.status(200).send('success')
+    }catch(err){
+        await conn.rollback()
         console.log(err)
+        return res.status(404).send('add error')
+    }finally{
+        conn.release()
+        console.log('finally')
     }
 })
 
-router.delete('/deleteRenter/:userId/:dorId/:rentId', async function(req, res, next){   
+router.delete('/deleteRenter/:userId/:dorId/:rentId', async (req, res, next) => {
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
+
     try{
-        const pay = await pool.query(
+        await pool.query(
             `delete from payment where renter_id = ? and dor_id = ?`, [req.params.rentId, req.params.dorId]
         );
 
-        const result = await pool.query(
+        await pool.query(
             `delete from renter where id = ? and dor_id = ?`, [req.params.rentId, req.params.dorId]
         );
-        console.log('success')
-        return res.json(result)
-    }catch (err){
+
+        conn.commit()
+        res.status(200).send('success')
+    }catch(err){
+        await conn.rollback()
         console.log(err)
+        return res.status(404).send('add error')
+    }finally{
+        conn.release()
+        console.log('finally')
     }
 })
 
@@ -234,7 +287,7 @@ router.delete('/deleteRenter/:userId/:dorId/:rentId', async function(req, res, n
 
 // payment ==========================================================================================================================
 
-router.get('/payment/:userId/:dorId/:date',upload.single(), async function(req, res, next){
+router.get('/payment/:userId/:dorId/:date',upload.single(), async (req, res, next) => {
     try{
         const result = await pool.query(
             `select * from dormitory where id = ?`, [req.params.dorId]
@@ -260,29 +313,47 @@ router.get('/payment/:userId/:dorId/:date',upload.single(), async function(req, 
     }
 })
 
-router.post('/addPayment/:userId/:dorId/:rentId',upload.single(), async function(req, res, next){
+router.post('/addPayment/:userId/:dorId/:rentId',upload.single(), async (req, res, next) => {
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
+
     try{
-        const result = await pool.query(
+        await pool.query(
             "insert into payment(dor_id, renter_id, num_room, date, water, light, status) VALUES(?, ?, ?, ?, ?, ?, ?)",
             [req.params.dorId, req.params.rentId, req.body.num_room, req.body.date, req.body.water, req.body.light, req.body.status]
         );
-        console.log('success')
-        return res.json(result)
-    }catch (err){
+
+        conn.commit()
+        res.status(200).send('success')
+    }catch(err){
+        await conn.rollback()
         console.log(err)
+        return res.status(404).send('add error')
+    }finally{
+        conn.release()
+        console.log('finally')
     }
 })
 
-router.put('/editStatus/:userId/:dorId/:rentId/:date',upload.single(), async function(req, res, next){
+router.put('/editStatus/:userId/:dorId/:rentId/:date',upload.single(), async (req, res, next) => {
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
+
     try{
-        const result = await pool.query(
+        await pool.query(
             `UPDATE payment SET status='1' where dor_id = ? and date = ? and renter_id =?`, 
             [req.params.dorId, req.params.date, req.params.rentId]
         );
-        console.log('success')
-        return res.json(result)
-    }catch (err){
+
+        conn.commit()
+        res.status(200).send('success')
+    }catch(err){
+        await conn.rollback()
         console.log(err)
+        return res.status(404).send('add error')
+    }finally{
+        conn.release()
+        console.log('finally')
     }
 })
 
