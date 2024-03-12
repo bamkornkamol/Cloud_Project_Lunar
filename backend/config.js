04a76ce878929762f2f0580ab02263148f855495
nickanyp <85357272+nickanyp@users.noreply.github.com>
2024-03-13 01:50:34 +0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'lunar-db.cjygw2mieoqy.ap-southeast-1.rds.amazonaws.com',
-  user: 'root',
-  password: '1234',
+  user: 'admin',
+  password: 'password',
   database: 'lunar',
   waitForConnections: true,
   connectionLimit: 10,
